b5b1d28141811482042908ed73c9caefb5d6e4a7
vsakun <vsakun@gmail.com>
2019-07-22 14:38:48 +0300

    update

--- berrycat_dj/blog/urls.py
+++ berrycat_dj/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', views.HomeView.as_view(), name='blog-home'),
     path('post/<int:id>/', views.DetailArticleView.as_view(), name='post'),
     path('search/', views.search, name='search'),
     path('articles/', views.AllArticleView.as_view(), name='articles'),
--- berrycat_dj/blog/views.py
+++ berrycat_dj/blog/views.py
@@ -1,14 +1,51 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Article
+from random import randint
 # from django.http import HttpResponse
 
 
-def home(request):
-    # data = {
-    #     'article': Article.objects.all(),
-    # }
-    return render(request, 'blog/index.html')
+class HomeView(ListView):
+    # model = Article
+    template_name = 'blog/index.html'
+    context_object_name = 'article'
+
+    # def get_context_data(self, **kwargs):
+    #     ctx = super(HomeView, self).get_context_data(**kwargs)
+    #     ctx['page_title'] = 'BerryCat'
+    #     return ctx
+
+    def get_queryset(self):
+
+        random_index = randint(0, Article.objects.count() - 4)
+
+        big_best_article = Article.objects.filter(
+            active=1).order_by('-like')[0]
+        small_best_article = Article.objects.filter(
+            active=1).order_by('-like')[1:5]
+        last_article = Article.objects.filter(
+            active=1).order_by('-date')[:9]
+        gallery_article = Article.objects.filter(
+            active=1).order_by('-date')[5:10]
+        random_article = Article.objects.filter(
+            active=1)[random_index:random_index + 4]
+        data = {
+            'big_best_article': big_best_article,
+            'small_best_article': small_best_article,
+            'last_article': last_article,
+            'gallery_article': gallery_article,
+            'random_article': random_article
+        }
+        return data
+    # def get_context_data(self, **kwargs):
+    #     ctx = super(HomeView, self).get_context_data(**kwargs)
+    #     ctx['big_best_article'] = Article.objects.filter(
+    #         active=1).order_by('-like')[0]
+    #     ctx['small_best_article'] = Article.objects.filter(
+    #         active=1).order_by('-like')[1:5]
+    #     ctx['last_article'] = Article.objects.filter(
+    #         active=1).order_by('-date')[:5]
+    #     return ctx
 
 
 def post(request):
