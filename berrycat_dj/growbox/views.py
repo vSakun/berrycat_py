3d657b6ff1dc0603c9e31e7245abc26dab43c370
vsakun <vsakun@gmail.com>
2019-08-22 16:22:02 +0300

    update

--- berrycat_dj/growbox/views.py
+++ berrycat_dj/growbox/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic import ListView, DetailView, UpdateView, TemplateView
 
 # Create your views here.
-class GrowView(DetailView):
-	pass
+class GrowView(TemplateView):
+	template_name = 'growbox/main.html'
+	context_object_name = 'data'
+
+
+	# def get_context_data(self):
+	# 	ctx = ({
+	# 		'title': 'GrowBox',
+	# 		'h1': 'Tommato'
+	# 		})
+	# 	return ctx
